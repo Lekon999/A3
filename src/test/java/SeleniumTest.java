76efae4c7f41b064fb62015c547f2fa30ab9ccdd
Кочнев Илья <Bankir369@gmail.com>
2023-05-27 14:50:08 +0300

    Inicial commit

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,30 +1,17 @@
-//import io.github.bonigarcia.wdm.WebDriverManager;
-//import org.junit.jupiter.api.AfterEach;
-//import org.junit.jupiter.api.BeforeAll;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.chrome.ChromeOptions;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static org.testng.AssertJUnit.assertEquals;
-
-class SeleniumTest {
-    private static WebDriver driver;
-    private org.openqa.selenium.By By;
-
+public class SeleniumTest {
+    private WebDriver driver;
 
     @BeforeAll
     static void setDriver() {
@@ -48,23 +35,16 @@ class SeleniumTest {
     }
 
     @Test
-    void shouldTest1() throws InterruptedException {
-
-        driver.get("http://localhost:9999/");
-        Thread.sleep(50000);
-        {
-
-            List<WebElement> inputs = driver.findElements(By.tagName("input"));
-            inputs.get(0).sendKeys("Иванов Иван");
-            inputs.get(1).sendKeys("+79099990099");
-            driver.findElement(By.className("checkbox__box")).click();
-            driver.findElement(By.className("button__text")).click();
-            String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайщее время.";
-            String actual = driver.findElement(By.className("order-success")).getText();
-            assertEquals(expected, actual);
-
-        }
+    void shouldReturnSuccessPage() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов Иван");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79022009911");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector(".button__text")).click();
+        String actual = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        assertEquals(expected, actual);
     }
+
 }
 
 
