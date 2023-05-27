081784ffa1d8bb852bbd7ce86394bf1a11d512ef
Кочнев Илья <Bankir369@gmail.com>
2023-05-27 11:46:34 +0300

    Inicial commit

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,10 +1,11 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeClass;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SeleniumTest {
     private static WebDriver driver;
@@ -19,39 +20,41 @@ class SeleniumTest {
         driver = new ChromeDriver(options);
     }
 
-//    @BeforeAll
-//    static void setUpAll() {
-//        System.setProperty("web-driver.chrome.driver", "./driver/win/chromedriver.exe");
-//    }
+    @BeforeAll
+    static void setUpAll() {
+        //WebDriverManager.chromedriver().setup();
+        System.setProperty("web-driver.chrome.driver", "./driver/win/chromedriver.exe");
+    }
 
-//    @BeforeEach
-//    void setUp() {
-//        driver = new ChromeDriver();
-//    }
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+    }
 
-//    @AfterEach
-//    void tearDown() {
-//        driver.quit();
-//        driver = null;
-//    }
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
 
     @Test
-    void shouldTest1() {
+    void shouldTest1() throws InterruptedException {
 
         driver.get("http://localhost:9999/");
-        {
-            driver.findElement(By).sendKeys("Иванов Иван");
-            driver.findElement(By).sendKeys("+79090909999");
-            driver.findElement(By).click();
-            driver.findElement(By).click();
-            String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайщее время.";
-            String actual = driver.findElement(By).getText();
-            assertEquals(expected, actual);
+        Thread.sleep(50000);
+//        {
+//
+//            List<WebElement> inputs = driver.findElements(By.tagName("input"));
+//            inputs.get(0).sendKeys("Иванов Иван");
+//            inputs.get(1).sendKeys("+79099990099");
+//            driver.findElement(By.className("checkbox__box")).click();
+//            driver.findElement(By.className("button__text")).click();
+//            String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайщее время.";
+//            String actual = driver.findElement(By.className("order-success")).getText();
+//            assertEquals(expected, actual);
 
-        }
     }
 }
-
 
 
 
